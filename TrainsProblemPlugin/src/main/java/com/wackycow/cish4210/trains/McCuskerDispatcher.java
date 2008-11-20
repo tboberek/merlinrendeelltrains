8cca2bea58ad30f93ee5a58a5a5779c5dc14de25
mccusker@gmail.com <mccusker@gmail.com@1321ffee-a080-11dd-9422-ed9963d0e455>
2008-11-20 01:43:21 +0000

    Additional route, first try at scheduler

--- TrainsProblemPlugin/src/main/java/com/wackycow/cish4210/trains/McCuskerDispatcher.java
+++ TrainsProblemPlugin/src/main/java/com/wackycow/cish4210/trains/McCuskerDispatcher.java
@@ -9,14 +9,28 @@ public class McCuskerDispatcher extends SchedulingDispatcher {
     public McCuskerDispatcher() {
     }
 
+    protected boolean containsStation(Train train, String station, Collection<Train> trains) {
+        for (Train t : trains) {
+            if (train == t) continue;
+            if (train.getRoute().contains(station)) return true;
+        }
+        return false;
+    }
+    
     @Override
     protected List<ScheduleItem> getSchedule(Collection<Train> trains) {
         List<ScheduleItem> result = new ArrayList<ScheduleItem>();
         for (Train t : trains) {
             List<String> route = t.getRoute();
-            for (int i=1; i< route.size(); ++i) {
-                result.add(new ScheduleItem(t, route.get(i), i));
+            for (int i=0; i< route.size(); ++i) {
+                if (i+1 < route.size() 
+                        && containsStation(t, route.get(i+1),trains))
+                    result.add(new ScheduleItem(t, route.get(i), i));
             }
+        }
+        System.out.println(getName()+" Created schedule:");
+        for (ScheduleItem item : result) {
+            System.out.println(item);
         }
         return result;
     }
